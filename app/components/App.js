618d0771b98fefce3eae25cf9bdc839cfeee3915
Murali Chepuri <33533809+mchepuri@users.noreply.github.com>
2023-03-17 01:39:01 -0700

    Plus redux (#37)
    * Added redux
    
    * Updated reducer name
    
    * Reducer export fixed
    
    * Made few things generic, menu and button labels , logos etc
    
    ---------
    
    Co-authored-by: mchepuri <mchepuri@schC02FK1PTMD6R.local>
    Co-authored-by: mchepuri <muralimanohar.chepuri.contractor@move.com>

--- app/components/App.js
+++ app/components/App.js
@@ -1,8 +1,27 @@
 import React,{Suspense} from 'react';
+import {Shell} from './layout/shell';
+import {Navigation} from './navigation/nav';
+import {IdentitySection} from './identity/identity';
+import { Hamburger } from './identity/hamburger/hamburger';
+import {Results} from './resultspage/results';
 import './app.css';
+import { Provider } from 'react-redux';
+import store from '../app_state/store';
+
+//import './nav.css';
 export  const App = () =>{
         return(
-        <div className="box"> Your components goes here</div>
+        <div className="app"> 
+                {/*Change me*/}
+                <Provider store={store}>
+                        <Shell logo={<img className="logo" src=""/>}
+                                menu={<Navigation/>}
+                                identitySection={ <IdentitySection/>}
+                                hamburger={<Hamburger/>}>
+                                <Results/>              
+                        </Shell>
+                </Provider>
+        </div>
         );
 }
 
